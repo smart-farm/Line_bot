9a9f423b1025dbcfad865e9847bc24ef64511e0d
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2018-10-27 18:06:08 +0700

    Add files via upload

--- Test.php
+++ Test.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Bangkok");
 $today=date("Y-m-d");
-if(time()>=strtotime("00:00:00") && time()<strtotime("00:00:00 + 4 hour"))
+/*if(time()>=strtotime("00:00:00") && time()<strtotime("00:00:00 + 4 hour"))
 {$today=date("Y-m-d",strtotime("-1 days",strtotime($today)));}*/
 include("connect.php");
          $sql="SELECT* from bot_puk where date='$today' ORDER BY bot_id DESC LIMIT 0,1";
