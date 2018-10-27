142226a8806c58c540e8773b907ff6b1afff879b
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2018-10-27 18:03:11 +0700

    Add files via upload

--- Test.php
+++ Test.php
@@ -1,8 +1,14 @@
 <?php
-$ms="line_puk.php?formula=p50&status=h";
-$requal = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/$ms");
-echo $requal;
-$content = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews.txt", "\xEF\xBB\xBF");
-echo $content;
-
+date_default_timezone_set("Asia/Bangkok");
+$today=date("Y-m-d");
+if(time()>=strtotime("00:00:00") && time()<strtotime("00:00:00 + 4 hour"))
+{$today=date("Y-m-d",strtotime("-1 days",strtotime($today)));}*/
+include("connect.php");
+         $sql="SELECT* from bot_puk where date='$today' ORDER BY bot_id DESC LIMIT 0,1";
+         $result=mysql_db_query($dbname,$sql);
+         $op=mysql_fetch_array($result);
+         $formula=$op[formula];
+         if ($formula=="")
+         { $formula="line_puk.php?formula=p50&status=h"; }
+echo $formula;
 ?>
