740399a222a8325bcacedcd96b81c987a8136aff
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2018-10-27 18:33:27 +0700

    Add files via upload

--- push.php
+++ push.php
@@ -17,28 +17,14 @@
 
 
    if($message == "เลขปัก"){
-     date_default_timezone_set("Asia/Bangkok");
-     $today=date("Y-m-d");
-     /*if(time()>=strtotime("00:00:00") && time()<strtotime("00:00:00 + 4 hour"))
-     {$today=date("Y-m-d",strtotime("-1 days",strtotime($today)));}*/
-     include("connect.php");
-     					$sql="SELECT* from bot_puk where date='$today' ORDER BY bot_id DESC LIMIT 0,1";
-     					$result=mysql_db_query($dbname,$sql);
-     					$op=mysql_fetch_array($result);
-     					$formula=$op[formula];
-     					if ($formula=="")
-     					{ $formula="line_puk.php?formula=p50&status=h"; }
-
-     //$requal = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/line_puk.php?formula=p50&status=h");*
-     //$ms="line_puk.php?formula=p50&status=h";
-     $requal = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/$formula");
+     $requal = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/push-puk.php");
      $content = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews.txt", "\xEF\xBB\xBF");
      echo $content;
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
       $arrayPostData['messages'][0]['text'] = $content;
-
       pushMsg($arrayHeader,$arrayPostData);
+     $requal = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/del-bot.php");
    }
    if($message == "เลขวิน"){
       $arrayPostData['to'] = $id;
