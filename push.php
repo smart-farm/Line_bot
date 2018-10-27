b8701521f8e7ffc39724d3fd46430fd0cae9f519
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2018-10-27 17:49:48 +0700

    Add files via upload

--- push.php
+++ push.php
@@ -15,13 +15,13 @@
    //$seal = file_get_contents('http://www.phonenana.com/yeekee/line.php?formula=b&status=shl');
    //$rood = file_get_contents('http://www.phonenana.com/yeekee/line.php?formula=bd&status=hl');
 
-   date_default_timezone_set("Asia/Bangkok");
-   $today=date("Y-m-d");
-   if(time()>=strtotime("00:00:00") && time()<strtotime("00:00:00 + 4 hour"))
-   {$today=date("Y-m-d",strtotime("-1 days",strtotime($today)));}
-   include("connect.php");
-   if($message == "เลขปัก"){
 
+   if($message == "เลขปัก"){
+     date_default_timezone_set("Asia/Bangkok");
+     $today=date("Y-m-d");
+     /*if(time()>=strtotime("00:00:00") && time()<strtotime("00:00:00 + 4 hour"))
+     {$today=date("Y-m-d",strtotime("-1 days",strtotime($today)));}*/
+     include("connect.php");
      					$sql="SELECT* from bot_puk where date='$today' ORDER BY bot_id DESC LIMIT 0,1";
      					$result=mysql_db_query($dbname,$sql);
      					$op=mysql_fetch_array($result);
