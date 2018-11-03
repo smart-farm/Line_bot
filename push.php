e8799d0a2be70a5ccaa8fc3da345038749d03b55
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2018-11-03 17:27:42 +0700

    Add files via upload

--- push.php
+++ push.php
@@ -108,7 +108,7 @@
    }
 
    if($message=="เลขตอง"){
-     if($id==$idfree||$id==$idvip||$id==$id199){
+     if($id==$idvip||$id==$id199||$id==$idfree||$id==$idsmile){
      $requal = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/remind.php");
      $content = file_get_contents("http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-tong.txt", "\xEF\xBB\xBF");
     // echo $content;
