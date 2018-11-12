8621c4e375f7707bed9c2b4828795aa1825c849f
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2018-11-13 00:22:09 +0700

    Add files via upload

--- push.php
+++ push.php
@@ -81,19 +81,22 @@ $id199 ='C678b1d0f7f216ba96cf8b6e784bac718';
 $idvip1='Cfee29536f26d44c081fe4eeb08a5400e';
 $idvip2='Cd95550591ae6ca7084af635c0d822c59';
 
-$mess="หวยรัฐบน";
-$formula =["เลขวิน","เลขรูด","เลขเสียว","เลขปัก","เลขไหล","เลขตอง","เลขวินบน","เลขวินล่าง"]; //,"เลขรูดบน","รูดบนล่าง"
+//$mess="หวยรัฐบน";
+$formula =["เลขวิน","เลขรูด","เลขเสียว","เลขปัก","เลขไหล","เลขตอง","เลขวินบน","เลขวินล่าง","เลขรูดบน","เลขรูดล่าง"];
 $url =["http://tornvidia.thddns.net:5152/easylotto/line-bot/push-win.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/push-rood.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/push-seal.php"
 ,"http://tornvidia.thddns.net:5152/easylotto/line-bot/push-puk.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/push-flow.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/remind.php"
-,"http://tornvidia.thddns.net:5152/easylotto/line-bot/push-win-on.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/push-win-lower.php"];
+,"http://tornvidia.thddns.net:5152/easylotto/line-bot/push-win-on.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/push-win-lower.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/push-rood-on.php"
+,"http://tornvidia.thddns.net:5152/easylotto/line-bot/push-rood-lower.php"];
 
 $url1=["http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-win.txt","http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-rood.txt","http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-rood.txt",
 "http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-puk.txt","http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-flow.txt","http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-tong.txt"
-,"http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-win.txt","http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-win.txt"];
+,"http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-win.txt","http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-win.txt","http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-rood.txt"
+,"http://tornvidia.thddns.net:5152/easylotto/line-bot/reviews-rood.txt"];
 
 $remove=["http://tornvidia.thddns.net:5152/easylotto/line-bot/del-win.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/del-rood.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/del-rood.php",
 "http://tornvidia.thddns.net:5152/easylotto/line-bot/del-bot.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/del-flow.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/del-tong.php",
-"http://tornvidia.thddns.net:5152/easylotto/line-bot/del-win.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/del-win.php"];
+"http://tornvidia.thddns.net:5152/easylotto/line-bot/del-win.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/del-win.php","http://tornvidia.thddns.net:5152/easylotto/line-bot/del-rood.php"
+,"http://tornvidia.thddns.net:5152/easylotto/line-bot/del-rood.php"];
 
 if($id==$id199||$id==$idvip1||$id==$idvip2||$id==$idadmin){
 for($i = 0; $i<count($formula);$i++) {
