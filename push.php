01f6c0797c61cee19c4f658f863deb5a52445930
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2018-12-03 19:58:11 +0700

    Add files via upload

--- push.php
+++ push.php
@@ -22,7 +22,7 @@ date_default_timezone_set("Asia/Bangkok");
  $today=date("Y-m-d");
 
 if($message=="เลขวิน"||$message=="เลขรูด"||$message=="เลขเสียว"||$message=="เลขปัก"||$message=="เลขเจาะ"){
- if($id==$idcheck}}$id==$idcheck1){ //||$id==$idcheck1
+ if($id==$idcheck||$id==$idcheck1){ //||$id==$idcheck1
 if(time()>=strtotime("22:00:00") && time()<strtotime("00:00:00")){
 
    $arrayPostData['to'] =$id;
