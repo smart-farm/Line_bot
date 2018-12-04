0596668a079a30c5b968bb5987977bc9ec6201d1
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2018-12-04 13:09:55 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,13 @@
+﻿<?php
+$host="easylotto.in.th";
+$user="root";
+$pw="Ae41121526";
+$dbname="phonenan_2016";
+$c=mysql_connect($host,$user,$pw);
+if (!$c)
+{
+	echo "ไม่สามารถเชื่อมต่อได้";
+	exit();
+}
+echo "เชื่อมต่อได้";
+?>
