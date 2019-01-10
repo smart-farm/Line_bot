9ee42ed93bdb9fb1fac9b6ea42542e25cb273650
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2019-01-11 01:30:56 +0700

    Add files via upload

--- push.php
+++ push.php
@@ -15,8 +15,8 @@ $userid = $arrayJson['events'][0]['source']['userId'];
 
 $idcheck ='C6158fb947c96653e2706ce8eb2dbae9b';
 $idcheck1 ='C000b66e767252bdc4efb43fb116d798e';
-$idfree ='C6158fb947c96653e2706ce8eb2dbae9b';
-$idfree2 ='C000b66e767252bdc4efb43fb116d798e';
+$idfree ='C6158fb947c96653e2706ce8eb2dbae9bxxx';
+//$idfree2 ='C000b66e767252bdc4efb43fb116d798e';
 //$idfree2 ='C000b66e767252bdc4efb43fb116d798e';
 date_default_timezone_set("Asia/Bangkok");
  $today=date("Y-m-d");
