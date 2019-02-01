e4e4032bd3cef31d1e5caa2ef081f4b55ac723c5
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2019-02-01 14:57:29 +0700

    Add files via upload

--- push-mawin.php
+++ push-mawin.php
@@ -10,6 +10,42 @@ $message = $arrayJson['events'][0]['message']['text'];
 //รับ id ของผู้ใช้
 $id = $arrayJson['events'][0]['source']['groupId'];
 $userid = $arrayJson['events'][0]['source']['userId'];
+
+if($arrayJson['events'][0]['type']=='memberJoined'){
+  $arrayPostData['to'] = $id;
+  $arrayPostData['messages'][0]['type'] = "text";
+  $arrayPostData['messages'][0]['text'] ="กฏกติกากลุ่มอีซี่ล็อตโต้
+=============
+1. ห้ามวางเลขหรือสูตรใดๆ
+2. ห้ามดึงบุคลอื่นเข้ามาในกลุ่ม
+3. ห้ามโพสรับสมาชิกหรือวางลิงค์
+4. ไม่อนุญาตให้โฆษณาใดๆ ทั้งสิ้น
+
+สมาชิกใหม่โปรดอ่าน
+=============
+• จ่ายเงิน/ต่ออายุ ไปที่นี่ : http://bit.ly/payment-est
+• เข้าโปรแกรม ไปที่นี่ : http://easylotto.in.th
+• คู่มือการใช้งาน >> http://bit.ly/2ThYqKv
+• การใช้งานโปรแกรมอื่นๆ >> http://bit.ly/2TmaoCU
+• เทคนิค/เคล็ดลับ การแทงเลขต่างๆ >> http://bit.ly/2S9WCpl
+• ติดต่อทีมงาน >> http://bit.ly/2MysQFJ
+";
+  pushMsg($arrayHeader,$arrayPostData);
+}
+if($arrayJson['events'][0]['type']=='memberLeft'){
+  $arrayPostData['to'] = $id;
+  $arrayPostData['messages'][0]['type'] = "text";
+  $arrayPostData['messages'][0]['text'] ="ขอบคุณที่ใช้บริการค่ะ";
+  $arrayPostData['messages'][1]['type'] = "sticker";
+  $arrayPostData['messages'][1]['packageId'] = "2";
+  $arrayPostData['messages'][1]['stickerId'] = "34";
+  pushMsg($arrayHeader,$arrayPostData);
+}
+
+
+
+
+
 if($message == "gettoken"){
     $arrayPostData['to'] = $id;
     $arrayPostData['messages'][0]['type'] = "text";
