b0fe8c69b4cfa033234bbff45b493e0c0104c82c
neetoo46 <42505489+neetoo46@users.noreply.github.com>
2019-02-08 15:25:34 +0700

    Add files via upload

---
+++ push-gun.php
@@ -0,0 +1,36 @@
+<?php
+$accessToken = "p7ZEY7QbLZiEwghQGs11ksNvflA328KkNezTJG8Oc030ojDXbilXtEFz7hnKzz2OdRa2ZHZk5zzVCTcijc0FTx/dkx0boRsiHW1ezLoWZ1pAlvrT4FiV9D9zjcZwx9JZvxKB1k4Jj2puk+9Uxny+DAdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+$content = file_get_contents('php://input');
+$arrayJson = json_decode($content, true);
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$accessToken}";
+//รับข้อความจากผู้ใช้
+$message = $arrayJson['events'][0]['message']['text'];
+//รับ id ของผู้ใช้
+$id = $arrayJson['events'][0]['source']['groupId'];
+$userid = $arrayJson['events'][0]['source']['userId'];
+if($message == "gettoken"){
+    $arrayPostData['to'] = $id;
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] =$id;
+    pushMsg($arrayHeader,$arrayPostData);
+}
+
+function pushMsg($arrayHeader,$arrayPostData){
+   $strUrl = "https://api.line.me/v2/bot/message/push";
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL,$strUrl);
+   curl_setopt($ch, CURLOPT_HEADER, false);
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   $result = curl_exec($ch);
+   curl_close ($ch);
+}
+exit;
+
+
+?>
